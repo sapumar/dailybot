9db715f34dd023e47aac7ac4509e2e6852b6c4ae
Alexandre Augusto <alexandrebarbaruiva@users.noreply.github.com>
2019-11-15 07:23:46 -0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,12 @@ HOUR = int(os.environ.get("HOUR", 10))
 MINUTE = int(os.environ.get("MINUTE", 00))
 DAILY_TIME = datetime.time(hour=HOUR, minute=MINUTE)
 
+# File locations
+START_FILE = "app/msg/start.md"
+DAILY_FILE = "app/msg/daily.md"
+EXAMPLE_FILE = "app/msg/example.md"
+ERROR_FILE = "app/msg/error.md"
+
 chat_ids = os.environ.get("CHAT_ID", "").replace(" ", "").split(",")
 
 # Port and link is given by Heroku/ngrok
